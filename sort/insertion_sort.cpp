87cfd98427dbc3b1a60c8cef524f416e12785437
PingZhengRIFO <78748224+PingZhengRIFO@users.noreply.github.com>
2023-04-24 17:19:02 -0400

    Create insertion_sort.cpp
    插入排序（Insertion Sort）是一种简单的排序算法。该算法每次将一个待排序的元素插入到已经排好序的有序序列中的适当位置，直到所有元素都排序完毕。
    
    下面是插入排序的具体实现步骤：
    
    将第一个元素视为已经排序的有序序列。
    从第二个元素开始，将该元素插入到已排序的序列中的正确位置，使得插入后仍然保持有序。
    重复步骤2，直到所有元素都插入到有序序列中。

---
+++ sort/insertion_sort.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void insertion_sort(int arr[], int n) {
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && key < arr[j]) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
+
+int main() {
+    int arr[] = {5, 2, 4, 6, 1, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Before sorting: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    insertion_sort(arr, n);
+
+    cout << "After sorting: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
